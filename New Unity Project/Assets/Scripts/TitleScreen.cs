9d53188b47bcc9057898a89c0d9c1d1e05e613ff
akill317 <375051821@qq.com>
2017-01-22 12:27:19 -0500

    Implemented 3 wave form

--- New Unity Project/Assets/Scripts/TitleScreen.cs
+++ New Unity Project/Assets/Scripts/TitleScreen.cs
@@ -15,10 +15,6 @@ public class TitleScreen : MonoBehaviour {
     // Update is called once per frame
     void Update() {
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            SceneManager.LoadScene(1);
-        }
-
     }
 
 
--- New Unity Project/Assets/Scripts/WaveMaker.cs
+++ New Unity Project/Assets/Scripts/WaveMaker.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Vectrosity;
+using DG.Tweening;
 
 public class WaveMaker : MonoBehaviour {
 
@@ -45,15 +46,18 @@ public class WaveMaker : MonoBehaviour {
 
     public int pointNum;
 
+    float squareWaveDuration;
+    float sawtoothDuration;
+    float triangleDuration;
+
     // Use this for initialization
-    void Start () {
+    void Start() {
         points = new float[pointNum];
         valuesMain = new Vector3[pointNum];
         valuesLeft = new Vector3[pointNum];
         valuesRight = new Vector3[pointNum];
 
-        for (int i = 0; i < pointNum; i++)
-        {
+        for (int i = 0; i < pointNum; i++) {
             pointsLS.Add(0);
             pointsRS.Add(0);
             points[i] = 0.0f;
@@ -62,17 +66,16 @@ public class WaveMaker : MonoBehaviour {
             valuesRight[i] = Vector3.zero;
         }
         InitLine();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
         MakeNewPoint();
         CombinePoints();
         DrawLine();
-	}
+    }
 
-    void MakeNewPoint ()
-    {
+    void MakeNewPoint() {
         float freqModLS = freqBaseLS + (freqRngLS * LSLR);
         float freqModRS = freqBaseRS + (freqRngRS * RSLR);
         float ampModLS = ampBaseLS + (ampRangeLS * LSUD);
@@ -81,8 +84,26 @@ public class WaveMaker : MonoBehaviour {
         periodLS += freqModLS * Time.fixedDeltaTime;
         periodRS += freqModRS * Time.fixedDeltaTime;
 
-        float nextPointLS = ampModLS * Mathf.Sin(periodLS);
-        float nextPointRS = ampModRS * Mathf.Sin(periodRS);
+        float l = Mathf.Sin(periodLS);
+        float r = Mathf.Sin(periodRS);
+
+        if (squareWaveDuration > 0) {
+            l = Mathf.Sin(periodLS) < 0 ? -1 : 1;
+            r = Mathf.Sin(periodLS) < 0 ? -1 : 1;
+        }
+
+        if (sawtoothDuration > 0) {
+            l = 1 - ((periodLS - Mathf.Floor(periodLS)) - 0.5f) * 2;
+            r = 1 - ((periodRS - Mathf.Floor(periodRS)) - 0.5f) * 2;
+        }
+
+        if (triangleDuration > 0) {
+            l = 2 / Mathf.PI * (float)System.Math.Asin(l);
+            r = 2 / Mathf.PI * (float)System.Math.Asin(r);
+        }
+
+        float nextPointLS = ampModLS * l;
+        float nextPointRS = ampModRS * r;
 
         pointsLS.Add(nextPointLS);
         pointsRS.Add(nextPointRS);
@@ -91,18 +112,14 @@ public class WaveMaker : MonoBehaviour {
         pointsRS.RemoveAt(0);
     }
 
-    void CombinePoints()
-    {
-        for (int i = 0; i < pointNum; i++)
-        {
+    void CombinePoints() {
+        for (int i = 0; i < pointNum; i++) {
             points[i] = (pointsLS[pointNum - 1 - i] + pointsRS[i]);
         }
     }
 
-    void DrawLine()
-    {
-        for (int i = 0; i < pointNum; i++)
-        {
+    void DrawLine() {
+        for (int i = 0; i < pointNum; i++) {
             valuesMain[i] = new Vector3(-pointNum + (i * 2), points[i], 0);
             valuesLeft[i] = new Vector3(-20 - (i * .8f), pointsLS[i] / 7 - 48, 0);
             valuesRight[i] = new Vector3(20 + (i * .8f), pointsRS[i] / 7 - 48, 0);
@@ -119,15 +136,28 @@ public class WaveMaker : MonoBehaviour {
         //graphicRight.Draw();
     }
 
-    public void InitLine()
-    {
+    public void SquareWave(float duration) {
+        squareWaveDuration = duration;
+        DOTween.To(() => squareWaveDuration, (x) => squareWaveDuration = x, 0, duration);
+    }
+
+    public void SawtoothWave(float duration) {
+        sawtoothDuration = duration;
+        DOTween.To(() => sawtoothDuration, (x) => sawtoothDuration = x, 0, duration);
+    }
+
+    public void TriangleWave(float duration) {
+        triangleDuration = duration;
+        DOTween.To(() => triangleDuration, (x) => triangleDuration = x, 0, duration);
+    }
+
+    public void InitLine() {
         graphicMain = new VectorLine("SquiggleMain", new List<Vector3>(segmentNum), null, lineWidth, LineType.Discrete, Joins.Weld);
         graphicLeft = new VectorLine("SquiggleLeft", new List<Vector3>(segmentNum), null, lineWidth, LineType.Discrete, Joins.Weld);
         graphicRight = new VectorLine("SquiggleRight", new List<Vector3>(segmentNum), null, lineWidth, LineType.Discrete, Joins.Weld);
     }
 
-    public void ShutDownLine()
-    {
+    public void ShutDownLine() {
         VectorLine.Destroy(ref graphicMain);
         VectorLine.Destroy(ref graphicLeft);
         VectorLine.Destroy(ref graphicRight);
