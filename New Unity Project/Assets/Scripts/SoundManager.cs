4d57d6da0699bfc308d857e2d321738dc3e208d3
Unknown <stupidlazybum@gmail.com>
2017-01-22 11:41:16 -0500

    ALEX: created sound manager

---
+++ New Unity Project/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour {
+
+	public int mySceneNum;
+
+	void Awake() {
+		SceneManager.sceneLoaded += NewScene;
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	void Start(){
+		
+	}
+
+	void NewScene (Scene scene, LoadSceneMode mode)
+	{
+		mySceneNum = scene.buildIndex;
+		Debug.Log ("This is scene #: " + scene.buildIndex);
+	}
+
+}
