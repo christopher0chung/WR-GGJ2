7223c691ebe2dd597c57e91ec1fac39b00f4348e
Chris Chung <christopher0chung@gmail.com>
2017-01-22 12:35:03 -0500

    Chris title

---
+++ New Unity Project/Assets/Scripts/CMoves.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMoves : MonoBehaviour {
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float newX = transform.localPosition.x - Time.fixedDeltaTime * 5;
+        if (newX <= -189)
+            newX += 378;
+        transform.localPosition = new Vector3(newX, transform.localPosition.y, transform.localPosition.z);
+	}
+}
