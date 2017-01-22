8f7e75a5e6ab5c1b595e03abac8555a1827d36fb
Robert Rappoport <rorappo@gmail.com>
2017-01-22 12:15:38 -0500

    ALEX: sound management, remove from timer

--- New Unity Project/Assets/Scripts/SoundManager.cs
+++ New Unity Project/Assets/Scripts/SoundManager.cs
@@ -6,20 +6,44 @@ using UnityEngine.SceneManagement;
 public class SoundManager : MonoBehaviour {
 
 	public int mySceneNum;
+	public AudioSource musicGenerator;
+
+
+
+	//AUDIO TRACKS
+	public AudioClip mainTheme;
+	public AudioClip titleTheme;
+	public AudioClip deadTheme;
 
 	void Awake() {
 		SceneManager.sceneLoaded += NewScene;
 		DontDestroyOnLoad(this.gameObject);
-	}
 
-	void Start(){
-		
+		musicGenerator = GetComponent<AudioSource> ();
+
 	}
 
 	void NewScene (Scene scene, LoadSceneMode mode)
 	{
 		mySceneNum = scene.buildIndex;
 		Debug.Log ("This is scene #: " + scene.buildIndex);
+
+
+		//THIS IS THE TITLE SCREEN
+		if (mySceneNum == 1) {
+			musicGenerator.volume = 1f;
+			musicGenerator.clip = titleTheme;
+		}
+
+
+		//LEVEL 1: PRESSING START LOADS SCENE 4 THEN 5 for some reason
+		if (mySceneNum == 4) {
+			musicGenerator.volume = 0.1f;
+			musicGenerator.clip = mainTheme;
+		}
+
+
+
 	}
 
 }
