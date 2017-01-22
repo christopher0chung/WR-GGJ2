0fd369c0fac4861fd8d953febc068e202c3c71fd
akill317 <375051821@qq.com>
2017-01-21 20:36:13 -0500

    Just use numDot instead of 100

--- New Unity Project/Assets/Scripts/WaveMaker.cs
+++ New Unity Project/Assets/Scripts/WaveMaker.cs
@@ -43,15 +43,16 @@ public class WaveMaker : MonoBehaviour {
     public Color leftLineCol;
     public Color rightLineCol;
 
-    // Use this for initialization
-    void Start () {
-        points = new float[100];
-        valuesMain = new Vector3[100];
-        valuesLeft = new Vector3[100];
-        valuesRight = new Vector3[100];
+    public int pointNum;
 
-        for (int i = 0; i < 100; i++)
-        {
+    // Use this for initialization
+    void Start() {
+        points = new float[pointNum];
+        valuesMain = new Vector3[pointNum];
+        valuesLeft = new Vector3[pointNum];
+        valuesRight = new Vector3[pointNum];
+
+        for (int i = 0; i < pointNum; i++) {
             pointsLS.Add(0);
             pointsRS.Add(0);
             points[i] = 0.0f;
@@ -60,17 +61,16 @@ public class WaveMaker : MonoBehaviour {
             valuesRight[i] = Vector3.zero;
         }
         InitLine();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
         MakeNewPoint();
         CombinePoints();
         DrawLine();
-	}
+    }
 
-    void MakeNewPoint ()
-    {
+    void MakeNewPoint() {
         float freqModLS = freqBaseLS + (freqRngLS * LSLR);
         float freqModRS = freqBaseRS + (freqRngRS * RSLR);
         float ampModLS = ampBaseLS + (ampRangeLS * LSUD);
@@ -89,19 +89,15 @@ public class WaveMaker : MonoBehaviour {
         pointsRS.RemoveAt(0);
     }
 
-    void CombinePoints()
-    {
-        for (int i = 0; i < 100; i++)
-        {
-            points[i] = (pointsLS[99 - i] + pointsRS[i]);
+    void CombinePoints() {
+        for (int i = 0; i < pointNum; i++) {
+            points[i] = (pointsLS[pointNum - 1 - i] + pointsRS[i]);
         }
     }
 
-    void DrawLine()
-    {
-        for (int i = 0; i < 100; i++)
-        {
-            valuesMain[i] = new Vector3(-100 + (i * 2), points[i], 0);
+    void DrawLine() {
+        for (int i = 0; i < pointNum; i++) {
+            valuesMain[i] = new Vector3(-pointNum + (i * 2), points[i], 0);
             valuesLeft[i] = new Vector3(-20 - (i * .8f), pointsLS[i] / 7 - 48, 0);
             valuesRight[i] = new Vector3(20 + (i * .8f), pointsRS[i] / 7 - 48, 0);
         }
@@ -113,19 +109,17 @@ public class WaveMaker : MonoBehaviour {
         graphicRight.color = rightLineCol;
 
         //graphicMain.Draw();
-        graphicLeft.Draw();
-        graphicRight.Draw();
+        //graphicLeft.Draw();
+        //graphicRight.Draw();
     }
 
-    public void InitLine()
-    {
+    public void InitLine() {
         graphicMain = new VectorLine("SquiggleMain", new List<Vector3>(segmentNum), null, lineWidth, LineType.Discrete, Joins.Weld);
         graphicLeft = new VectorLine("SquiggleLeft", new List<Vector3>(segmentNum), null, lineWidth, LineType.Discrete, Joins.Weld);
         graphicRight = new VectorLine("SquiggleRight", new List<Vector3>(segmentNum), null, lineWidth, LineType.Discrete, Joins.Weld);
     }
 
-    public void ShutDownLine()
-    {
+    public void ShutDownLine() {
         VectorLine.Destroy(ref graphicMain);
         VectorLine.Destroy(ref graphicLeft);
         VectorLine.Destroy(ref graphicRight);
