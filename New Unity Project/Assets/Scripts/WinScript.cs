876aba8b75d69fb84f34df399ff3ce87bf71a569
akill317 <375051821@qq.com>
2017-01-21 20:17:49 -0500

    clean of winscript and title Screen

--- New Unity Project/Assets/Scripts/WinScript.cs
+++ New Unity Project/Assets/Scripts/WinScript.cs
@@ -4,33 +4,33 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class WinScript : MonoBehaviour {
 
-	public bool sceneIsOver = false;
-	public bool timeIsUp = false;
-	public bool playerDied = false;
-	public int score = 0;
+    public bool sceneIsOver = false;
+    public bool timeIsUp = false;
+    public bool playerDied = false;
+    public int score = 0;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start() {
 
-	//	if (timeIsUp == true) {
-	//		if (score > 85) {
-	//			//go to next stage
-	//			//unload previous stage
-	//		} else {
-	//			//go to try again screen
-	//		}
-	//	}
-	//if (playerDied == true) {
-	//		//go to try again screen
-			
-	//	}
-	//}
+    }
 
-	}
-		
+    // Update is called once per frame
+    void Update() {
 
+        //	if (timeIsUp == true) {
+        //		if (score > 85) {
+        //			//go to next stage
+        //			//unload previous stage
+        //		} else {
+        //			//go to try again screen
+        //		}
+        //	}
+        //if (playerDied == true) {
+        //		//go to try again screen
+
+        //	}
+        //}
+
+    }
+
+}
