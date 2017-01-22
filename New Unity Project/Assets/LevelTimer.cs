660515b60d95d1d9d606327aa5c65c095a26827b
Chris Chung <christopher0chung@gmail.com>
2017-01-21 21:20:24 -0500

    Win script is working.

---
+++ New Unity Project/Assets/LevelTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTimer : MonoBehaviour {
+
+    public float levelDuration;
+
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (timer >= levelDuration)
+        {
+            GameObject.FindGameObjectWithTag("TextController").GetComponent<WinScript>().timeIsUp = true;
+        }
+
+	}
+}
